80ef95da230da7ebd876cb49f6a63ad5f4ab2524
Ao Song <ao.song@outlook.com>
2018-08-02 17:24:50 +0200

    little by little

---
+++ protos/gen.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+protoc --cpp_out=./src/ ./auth.proto
+protoc --cpp_out=./src/ ./kv.proto
+protoc --cpp_out=./src/ ./service.proto
+protoc --cpp_out=./src/ ./rpc.proto
+protoc --grpc_out=./src --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./rpc.proto
